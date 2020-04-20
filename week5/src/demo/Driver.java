464252df7451480fe9b594c53226cfbb8a8a742e
Jiawei Wu <jiaweiwu@macdeMacBook-Pro.local>
2020-04-20 15:20:58 +1000

    123

--- week5/src/demo/Driver.java
+++ week5/src/demo/Driver.java
@@ -2,6 +2,16 @@ package demo;
 
 public class Driver {
     public static void main(String[] args) {
-        System.out.println(123);
+        Appointment appointment = new Appointment(2020,4,20,15,16);
+        System.out.println(appointment);
+
+
+        int[] myArray = new int[100];
+
+
+        Appointment[] myArray1 = new Appointment[100];
+        for(int i = 0; i < 100; i++){
+            myArray1[i] = new Appointment();
+        }
     }
 }

f31f660beb0588de1fc72e6890310d86a472f6d7
Jiawei Wu <jiaweiwu@WJWsMacBook-Pro.local>
2020-08-12 14:59:35 +1000

    add MyProgram

---
+++ week2/MyProgram.java
@@ -0,0 +1,18 @@
+public class MyProgram {
+    //psvm
+    public static void main(String[] args){
+        String str1;
+        str1 = "So Long!";
+
+        // String str1 = "Hello World";
+
+        System.out.println(str1);
+
+        String str2;
+        str2 = str1.toUpperCase();
+
+        //sout
+        System.out.println(str2);
+
+    }
+}
